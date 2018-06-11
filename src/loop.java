613e57bd1ffd7c8e022493396bf6e115454f0b9d
Abir CSE <abircoxsbazar@gmail.com>
2018-06-11 12:12:29 +0600

    Loop Test

---
+++ src/loop.java
@@ -0,0 +1,19 @@
+public class loop {
+
+    public static void  main (String[] arg)
+    {
+        int a = 0;
+        int result;
+
+        for (int i = 1 ; i<=100; i++)
+        {
+
+            result = a+i;
+            System.out.println(result);
+
+        }
+
+
+
+    }
+}
