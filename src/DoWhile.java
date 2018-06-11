571033a1207f183a31da45e74351a66cab6480f9
Abir CSE <abircoxsbazar@gmail.com>
2018-06-11 12:30:28 +0600

    Do While loop added

---
+++ src/DoWhile.java
@@ -0,0 +1,16 @@
+public class DoWhile {
+
+    public static void  main (String[] arg)
+    {
+        int a = 0;
+
+        do {
+
+            System.out.println("OUR INITIALIZE VALUE IS: "+a);
+            a++;
+        }
+        while (a <= 50);
+
+
+    }
+}
