cb095fb451ea4331f24ab4e1c1135c354866d956
Abir CSE <abircoxsbazar@gmail.com>
2018-06-11 12:20:23 +0600

    While loop added

---
+++ src/While.java
@@ -0,0 +1,15 @@
+public class While {
+
+    public static void  main (String[] arg)
+    {
+        int value = 1;
+
+        while(value <= 20)
+        {
+            System.out.println(value);
+            value++;
+        }
+
+
+    }
+}
